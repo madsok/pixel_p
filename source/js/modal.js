93b35e721e4b0215437e4cfdab9a05c30405e96c
madsok <46710526+madsok@users.noreply.github.com>
2020-02-03 17:27:37 +0300

    js, improvement html and css

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,14 +1,36 @@
-var contactsLink = document.querySelector(".contacts__link"),
-    modal = document.querySelector(".modal-wrapper"),
-    userName = modal.querySelector("[name=name]"),
-    userTel = modal.querySelector("[name=tel]"),
-    userEmail = modal.querySelector("[name=email]"),
-    modalForm = modal.querySelector(".modal__form"),
-    modalInput = modal.querySelector(".modal__input"),
-    modalClose = document.querySelector(".modal__close");
+'use strict';
 
-contactsLink.addEventListener("click", function (evt) {
+  var contactsLink = document.querySelector(".contacts__link"),
+      modal = document.querySelector(".modal-wrapper"),
+      modalForm = modal.querySelector(".modal__form"),
+      modalClose = document.querySelector(".modal__close");
+
+  contactsLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.add("modal-wrapper--active");
+  });
+
+  modalForm.button.onclick = function () {
+    for (var i = 0; i < modalForm.elements.length; i++) {
+      if (modalForm.elements[i].value === '' && modalForm.elements[i].hasAttribute('required')) {
+        modalForm.elements[i].placeholder = "Поле обязательно для заполнения";
+        modalForm.elements[i].classList.add("modal__input--invalid");
+        modalForm.elements[i].classList.add("modal__input--invalid::placeholder");
+        return false;
+      }
+    }
+  };
+
+  window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal-wrapper--active")) {
+      modal.classList.remove("modal-wrapper--active");
+    }
+  }
+})
+
+  modalClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.add("modal-wrapper--active");
-  userName.focus();
-});
+  modal.classList.remove("modal-wrapper--active");
+})
